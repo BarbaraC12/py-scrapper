25cd422910b63e21dacc9b1530c391033eb33b75
bcano <cano.barbara@outlook.fr>
2023-11-08 21:54:21 +0100

    basic api parser

--- parserAPI.py
+++ parserAPI.py
@@ -12,18 +12,27 @@ from decouple import config
 #   "https://developers.giphy.com/"
 #   }
 # https://talks.freelancerepublik.com/api-publiques-gratuites-developpement-site-app/
-api_url = config('API_URL')
-
-auth_data = {
-    'uid': config('API_UID'),
-    'secret': config('API_SECRET')
-}
-
-
-response = requests.get(api_url, params=auth_data) if auth_data['uid'] or auth_data['secret'] else requests.get(api_url)
-
-if response.status_code != 200:
-    print("Failed to get data from API")
-else:
+try:
+    api_url = config('API_URL')
+    if api_url:
+        auth_data = {
+            'uid': config('API_UID'),
+            'secret': config('API_SECRET')
+        }
+    else:
+        raise ValueError("No API found check .env file")
+    response = requests.get(api_url, params=auth_data) if auth_data['uid'] or auth_data['secret'] else requests.get(api_url)
+    response.raise_for_status()  # Check for HTTP status code errors
+    if response.status_code != 200:
+        raise ValueError(f"Failed and get info from {api_url} API.\n")
     data = response.json()
     print(data)
+except requests.exceptions.ConnectionError as ce:
+    print(f"Connection Error: {ce}\n",
+          "Check yout connection. End of program.")
+except requests.exceptions.RequestException as re:
+    print(f"Request Exception: {re}")
+except ValueError as e:
+    print(f"Error: {e}")
+except KeyboardInterrupt:
+    print("\nInput interrupted. End of program.")
